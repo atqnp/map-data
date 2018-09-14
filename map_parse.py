5fd4881d14634fab695c51c9c52bf29c3a840156
atqnp <atiq_np@hotmail.com>
2018-09-14 11:47:41 +0900

    Update map_parse.py

--- map_parse.py
+++ map_parse.py
@@ -1,5 +1,8 @@
+import xml.etree.ElementTree as ET
+import pandas as pd
 from bs4 import BeautifulSoup as Soup
 from bs4 import Tag
+maketrans = str.maketrans
 
 file = "tokyo.kml"
 
@@ -12,30 +15,39 @@ dataplace = soup.find_all("ExtendedData")
 
 coor_tag = soup.find_all("coordinates")
 no_tag = soup.find_all("LinearRing")
+schema_tag = soup.find_all("SchemaData")
 
 for tag in coor_tag:
     tag.unwrap()
 for tag in no_tag:
     tag.unwrap()
-    
+for tag in schema_tag:
+    tag.unwrap()
+                
 for place in dataplace:
-    subplace = place.text
-    final = subplace.split("\n")
-    datalist.append(final)
+    data1 = []
+    data2 = []
+    for sub in place.find_all("SimpleData"):
+        subname = sub['name']
+        subplace = sub.text
+        data1.append(subname)
+        data2.append(subplace)
+    datadict = dict(zip(data1,data2))
+    datalist.append(datadict)
     
 for place in dataplace:
     next_sib = place.next_sibling
     while(not isinstance(next_sib, Tag)):
         next_sib = next_sib.next_sibling
     fin_coor = str(next_sib)
-    test = fin_coor.replace('<MultiGeometry>','(').replace('</MultiGeometry>',')').replace('<outerBoundaryIs>','(').replace('</outerBoundaryIs>',')').replace('<innerBoundaryIs>','(').replace('</innerBoundaryIs>',')').replace('<Polygon>','(').replace('</Polygon>',')')
+    fin_coor = fin_coor.translate(maketrans(', ',' ,'))
+    test = fin_coor.replace('<MultiGeometry><Polygon>','MULTIPOLYGON ((').replace('</MultiGeometry>',')').replace('<outerBoundaryIs>','(').replace('</outerBoundaryIs>',')').replace('<innerBoundaryIs>',',(').replace('</Polygon><Polygon>','),(').replace('</innerBoundaryIs>',')').replace('<Polygon>','POLYGON (').replace('</Polygon>',')')
     coorlist.append(test)
 
-
 #Pandas
-import pandas as pd
 df1 = pd.DataFrame(datalist)
 df2 = pd.DataFrame(coorlist)
-
+df2.columns = ["GEOMETRY"]
 findata = pd.concat([df1,df2], axis = 1)
-findata
+findata = findata[['KEN','GUN','SIKUCHOSON','JCODE','P_NUM','H_NUM','GEOMETRY']]
+findata.to_csv("findata.csv")
