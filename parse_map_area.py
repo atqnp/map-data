9b17345b09e86677b0443c9b15c3a1715599072e
atqnp <atiq_np@hotmail.com>
2018-09-20 17:06:19 +0900

    Update parse_map_area.py

--- parse_map_area.py
+++ parse_map_area.py
@@ -1,10 +1,9 @@
-import xml.etree.ElementTree as ET
 import pandas as pd
 from bs4 import BeautifulSoup as Soup
 from bs4 import Tag
 maketrans = str.maketrans
 
-for i in range(46,-1,-1):
+for i in range(41,-1,-1):
     f_num = format(i+1,'02d')
     file = ("{}_area.kml".format(f_num))
 
@@ -44,17 +43,17 @@ for i in range(46,-1,-1):
         fin_coor = str(next_sib)
         fin_coor = fin_coor.replace("\n", "").replace(" ","").replace(",100"," ")
         fin_coor = fin_coor.translate(maketrans(', ',' ,'))
-        test = fin_coor.replace('<MultiGeometry><Polygon>','MULTIPOLYGON ((').replace('</MultiGeometry>',')').replace('<outerBoundaryIs>','(').replace('</outerBoundaryIs>',')').replace('<innerBoundaryIs>',',(').replace('</Polygon><Polygon>','),(').replace('</innerBoundaryIs>',')').replace('<Polygon>','POLYGON (').replace('</Polygon>',')').replace(',)',')')
+        test = fin_coor.replace('<outerBoundaryIs>','(').replace('</outerBoundaryIs>',')').replace('<innerBoundaryIs>',',(').replace('</Polygon><Polygon>','),(').replace('</innerBoundaryIs>',')').replace('<Polygon>','POLYGON (').replace('</Polygon>',')').replace(',)',')')
         coorlist.append(test)
 
-        #Pandas
-        f_csv = ("zendata_{}_area.csv".format(f_num))
-        f_excel = ("zendata_{}_area.xlsx".format(f_num))
-        df1 = pd.DataFrame(datalist)
-        df2 = pd.DataFrame(coorlist)
-        df1 = df1[['PREF_NAME','CITY_NAME','S_NAME']]
-        df2.columns = ["GEOMETRY"]
-        zendata = pd.concat([df1,df2], axis = 1)
-        zendata.to_csv(f_csv)
+    #Pandas
+    f_csv = ("zendata_{}_area.csv".format(f_num))
+    f_excel = ("zendata_{}_area.xlsx".format(f_num))
+    df1 = pd.DataFrame(datalist)
+    df2 = pd.DataFrame(coorlist)
+    df1 = df1[['PREF_NAME','CITY_NAME','S_NAME']]
+    df2.columns = ["GEOMETRY"]
+    zendata = pd.concat([df1,df2], axis = 1)
+    zendata.to_csv(f_csv)
 
     print("done_area_{}".format(f_num))
